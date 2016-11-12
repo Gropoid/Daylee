426df7f3f9943b06e46b2d9370ede13251ccf502
geraud <geraud.floodme@gmail.com>
2016-11-12 19:50:44 +0100

    Setup Kotlin Create 3 core activities

---
+++ app/src/main/kotlin/gropoid/daylee/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package gropoid.daylee
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DetailsActivity : AppCompatActivity() {
+
+    override protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
---
+++ app/src/main/kotlin/gropoid/daylee/WorkdayActivity.kt
@@ -0,0 +1,12 @@
+package gropoid.daylee
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WorkdayActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workday)
+    }
+}
